58fbe4c6fb04229127b92de0f77ebb8adf850010
Arif Muchiyar Rahman <arif.muchiyar@gmail.com>
2023-03-28 09:39:16 +0700

    first commit

---
+++ Test Suites/shopday.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>shopday</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>503e18d4-ab9a-4bf5-8bc3-fb379e35d5ce</testSuiteGuid>
+   <testCaseLink>
+      <guid>e8e27ac8-aec0-4465-a875-c7e6cd9b63c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shop/FlashSale</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
